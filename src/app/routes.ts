e3bf74fae5efc90d7971278fd51ea2f6adad013f
don Pablo <don@obeyi.com>
2020-10-26 08:20:22 +0200

    Create Pages
    Signed-off-by: don Pablo <don@obeyi.com>

---
+++ src/app/routes.ts
@@ -0,0 +1,28 @@
+import {Routes} from "@angular/router";
+import {HomeComponent} from "./pages/home/home.component";
+import {ArtistComponent} from "./pages/artist/artist.component";
+import {AlbumComponent} from "./pages/album/album.component";
+
+export const ROUTES: Routes = [
+  {
+    path: '',
+    redirectTo: 'search',
+    pathMatch: 'full'
+  },
+  {
+    path: 'search',
+    component: HomeComponent
+  },
+  {
+    path: 'artist/:artistSlug',
+    component: ArtistComponent
+  },
+  {
+    path: 'artist/:artistSlug/album/:albumSlug',
+    component: AlbumComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
