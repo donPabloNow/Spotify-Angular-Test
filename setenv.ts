299c125b4c4b5bd3a0a20ea89f49bcb4095c6239
don Pablo <don@obeyi.com>
2020-10-26 08:14:07 +0200

    API Key Storage
    Signed-off-by: don Pablo <don@obeyi.com>

---
+++ setenv.ts
@@ -0,0 +1,33 @@
+const { writeFile } = require('fs');
+const { argv } = require('yargs');
+
+// read environment variables from .env file
+require('dotenv').config();
+
+// read the command line argument passed
+// with yargs
+const environment = argv.environment;
+const isProduction = environment === 'prod';
+
+const targetPath = isProduction
+  ? `./src/environments/environment.prod.ts`
+  : `./src/environments/environment.ts`;
+
+// we have access to our environment variables
+// in the process.env object thanks to dotenv
+const environmentFileContent = `
+export const environment = {
+  production: ${isProduction},
+  spotify_client_id: "${process.env.spotify_client_id}",
+  spotify_client_secret: "${process.env.spotify_client_secret}" 
+};
+`;
+
+// write the content to the respective file
+writeFile(targetPath, environmentFileContent, function (err) {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log(`Wrote variables to ${targetPath}`);
+});
