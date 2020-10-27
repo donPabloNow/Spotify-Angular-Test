9284b82632bbdd4c283786ab5a7c6655954bb71e
don Pablo <don@obeyi.com>
2020-10-27 09:45:53 +0200

    Signed-off-by: don Pablo <don@obeyi.com>

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-
-export const environment = {
-  production: false,
-  spotify_client_id: "b09d0482cf53461c9ee066666983dcc6",
-  spotify_client_secret: "639b40801ebb40cfa92e64bf170b9539" ,
-  spotify_page_size: 30
-};
